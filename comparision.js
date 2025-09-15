d3fa70404c4a06b50e4923cf48dd4dbfd1143fc8
AbhijatPatel <abhijatpatelfaizabad@gmail.com>
2025-09-15 23:36:10 +0530

    comparision  operators in JS.

---
+++ comparision.js
@@ -0,0 +1,37 @@
+// Comparison Operators.
+
+//equal to (==)
+
+let a=5;
+let b=5;
+
+console.log("a==b",a==b); //true
+
+
+//Not equal to (!=)
+
+console.log("a!=b",a!=b);//false
+
+//Equal to & type (===)
+
+console.log("a===b",a===b);//true
+
+//Not equal to & type (!==)
+
+console.log("a!==b",a!==b);//false
+
+//Greater than (>)
+
+console.log("a>b",a>b);//false
+
+//Less than (<)
+
+console.log("a<b",a<b);//false
+
+//Greater than or equal to (>=)
+
+console.log("a>=b",a>=b);//true
+
+//Less than or equal to (<=)
+
+console.log("a<=b",a<=b);//true
