ae1fd4a0029899b229ca147993e8c35c379f87b5
ABHIJAT PATEL <abhijatpatelfaizabad@gmail.com>
2025-09-15 23:35:08 +0530

    Delete JavaScript directory

--- JavaScript/operators.js
+++
@@ -1,6 +0,0 @@
-//Ternary Operator
-let age = 20;
-let eligibility = (age >= 18) ? "Eligible to vote" : "Not eligible to vote"; // condition ? true output : false output
-console.log(eligibility);
-
-
